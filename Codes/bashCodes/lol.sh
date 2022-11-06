bc2722f136d3a3a8778c39d26bbddbb7c3f64c6f
marcomarchesiskywarder <marco.marchesi@skywarder.eu>
2022-11-06 11:05:59 +0100

    update first order SA mesh 3-8

--- Codes/bashCodes/lol.sh
+++ Codes/bashCodes/lol.sh
@@ -4,6 +4,19 @@ read -p "Input the mesh numbering starting index: " meshIndex
 read -p "Input the mesh numbering ending index: " meshNumber
 read -p "Input the maximum number of cores allowed: " maxCoreNumber
 read -p "Input tha name of the template configuration file: " templateName
+read -p "Select either first or second order: " simulationOrder
+
+if [ $simulationOrder -eq 2 ] ; then
+
+    echo "Second order analysis selected!"
+    TVD=()
+
+    for jj in $(seq $meshIndex 1 $meshNumber) ; do
+
+        read -p "Input the TVD coefficient for meshG${jj}: " TVD[$jj]
+
+    done  
+fi
 
 for jj in $(seq $meshIndex 1 $meshNumber) ; do 
 	
@@ -18,7 +31,7 @@ for jj in $(seq $meshIndex 1 $meshNumber) ; do
 	cd ..
 	
 	# Copying the mesh folder inside test case folder
-	cp -r "../../../../MeshFiles/meshG${jj}" $caseFolderName
+	cp -r "../../../../../MeshFiles/meshG${jj}" $caseFolderName
 	
 	# Generating simulation recup file
 	cp $templateName $caseFolderName
@@ -31,25 +44,35 @@ for jj in $(seq $meshIndex 1 $meshNumber) ; do
 	searchString="MESH_FILENAME= ../mesh/mesh.su2"
 	replaceString="MESH_FILENAME= ../meshG${jj}/meshG${jj}.su2"
 	sed -i "s%$searchString%$replaceString%" $caseName
-	
+
 	# Updating output files name
 	sed -i "s/CONV_FILENAME= history/CONV_FILENAME= history_G${jj}/" $caseName
+
 	sed -i "s/RESTART_FILENAME= restart_flow.dat/RESTART_FILENAME= restart_flow_G${jj}.dat/" $caseName
 	sed -i "s/VOLUME_FILENAME= flow/VOLUME_FILENAME= flow_G${jj}/" $caseName
 	sed -i "s/SURFACE_FILENAME= surface_flow/SURFACE_FILENAME= surface_flow_G${jj}/" $caseName
+
 	sed -i "s/MESH_OUT_FILENAME= mesh_out.su2/MESH_OUT_FILENAME= mesh_out_G${jj}.su2/" $caseName
-	
+
+    if [ $simulationOrder -eq 2 ] ; then
+        	sed -i "s/VENKAT_LIMITER_COEFF= 0.01/VENKAT_LIMITER_COEFF= ${TVD[$jj]}/" $caseName
+		    sed -i "s/SOLUTION_FILENAME= restart_solution.dat/RESTART_FILENAME= ../../../O1/caseG${jj}/cfdG${jj}/restart_flow_G${jj}.dat/" $caseName
+	fi
+
 	# Computing the optimal core number
 	searchDir=../meshG${jj}/meshG${jj}.su2
+
 	elemNumber=$(sed -n 's/^NELEM= \(.*\)/\1/p' < $searchDir)
+
 	optimalCoreNumber=$(echo "scale=0;($elemNumber+10000)/20000" | bc)
+
 	if [ $optimalCoreNumber -gt $maxCoreNumber ] ; then
 		optimalCoreNumber=$maxCoreNumber
 	fi
 	
 	# Running SU2
 	echo Starting simulation n. ${jj} of $meshNumber 
-	mpirun -n 6 --allow-run-as-root SU2_CFD $caseName # >"logG${jj}.log"
+	mpirun -n 6 --allow-run-as-root SU2_CFD $caseName >"logG${jj}.log"
 	
 	# Back to master folder
 	cd ..
