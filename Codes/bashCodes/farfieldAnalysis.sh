bc945b380ff7761dc28e8f816de25b04eaae7dd0
marcomarchesiskywarder <marco.marchesi@skywarder.eu>
2022-11-15 09:17:50 +0100

    some simulations done, check oneDrive for results

---
+++ Codes/bashCodes/farfieldAnalysis.sh
@@ -0,0 +1,100 @@
+echo "Mesh convergence initialized"
+
+read -p "Input the mesh numbering index: " meshIndex
+read -p "Input the maximum number of cores allowed: " maxCoreNumber
+read -p "Input tha name of the template configuration file: " templateName
+read -p "Input the TVD for the second order simulation: " secondOrderTVD
+
+# Generating the test case folders
+caseFolderNameO1="O1"
+mkdir $caseFolderNameO1  
+caseFolderNameO2="O2"
+mkdir $caseFolderNameO2
+
+##################################################################################################################### ORDER ONE
+# Generating the cfd folder
+cd $caseFolderNameO1
+cfdFolderNameO1="cfdG${meshIndex}"
+mkdir $cfdFolderNameO1  
+cd ..
+
+# Copying the mesh folder inside test case folder
+cp -r "../../../../../MeshFiles/meshG${meshIndex}" $caseFolderNameO1
+
+# Generating simulation recup file
+cp $templateName $caseFolderNameO1
+cd $caseFolderNameO1
+caseNameO1="simulationCase_G${meshIndex}.cfg"
+mv $templateName $cfdFolderNameO1/$caseNameO1
+
+# Updating the mesh input in the test case file
+cd $cfdFolderNameO1
+searchString="MESH_FILENAME= ../mesh/mesh.su2"
+replaceString="MESH_FILENAME= ../meshG${meshIndex}/meshG${meshIndex}.su2"
+sed -i "s%$searchString%$replaceString%" $caseNameO1
+
+# Updating output files name
+sed -i "s/CONV_FILENAME= history/CONV_FILENAME= history_G${meshIndex}/" $caseNameO1
+sed -i "s/RESTART_FILENAME= restart_flow.dat/RESTART_FILENAME= restart_flow_G${meshIndex}.dat/" $caseNameO1
+sed -i "s/VOLUME_FILENAME= flow/VOLUME_FILENAME= flow_G${meshIndex}/" $caseNameO1
+sed -i "s/SURFACE_FILENAME= surface_flow/SURFACE_FILENAME= surface_flow_G${meshIndex}/" $caseNameO1
+sed -i "s/MESH_OUT_FILENAME= mesh_out.su2/MESH_OUT_FILENAME= mesh_out_G${meshIndex}.su2/" $caseNameO1
+
+# Computing the optimal core number
+searchDir=../meshG${meshIndex}/meshG${meshIndex}.su2
+elemNumber=$(sed -n 's/^NELEM= \(.*\)/\1/p' < $searchDir)
+optimalCoreNumber=$(echo "scale=0;($elemNumber+10000)/20000" | bc)
+if [ $optimalCoreNumber -gt $maxCoreNumber ] ; then
+	optimalCoreNumber=$maxCoreNumber
+fi
+
+# Running SU2
+echo Starting O1 simulation... 
+echo Running with $optimalCoreNumber cores
+mpirun -n $optimalCoreNumber SU2_CFD $caseNameO1 #>"logG${meshIndex}.log"
+
+# Back to master folder
+cd ..
+cd ..
+
+##################################################################################################################### ORDER TWO
+# Generating the cfd folder
+cd $caseFolderNameO2
+cfdFolderNameO2="cfdG${meshIndex}"
+mkdir $cfdFolderNameO2 
+cd ..
+
+# Copying the mesh folder inside test case folder
+cp -r "../../../../../MeshFiles/meshG${meshIndex}" $caseFolderNameO2
+
+# Generating simulation recup file
+cp $templateName $caseFolderNameO2
+cd $caseFolderNameO2
+caseNameO2="simulationCase_G${meshIndex}.cfg"
+mv $templateName $cfdFolderNameO1/$caseNameO2
+
+# Updating the mesh input in the test case file
+cd $cfdFolderNameO2
+searchString="MESH_FILENAME= ../mesh/mesh.su2"
+replaceString="MESH_FILENAME= ../meshG${meshIndex}/meshG${meshIndex}.su2"
+sed -i "s%$searchString%$replaceString%" $caseNameO2
+
+# Updating output files name
+sed -i "s/CONV_FILENAME= history/CONV_FILENAME= history_G${meshIndex}/" $caseNameO2
+sed -i "s/RESTART_FILENAME= restart_flow.dat/RESTART_FILENAME= restart_flow_G${meshIndex}.dat/" $caseNameO2
+sed -i "s/VOLUME_FILENAME= flow/VOLUME_FILENAME= flow_G${meshIndex}/" $caseNameO2
+sed -i "s/SURFACE_FILENAME= surface_flow/SURFACE_FILENAME= surface_flow_G${meshIndex}/" $caseNameO2
+sed -i "s/MESH_OUT_FILENAME= mesh_out.su2/MESH_OUT_FILENAME= mesh_out_G${meshIndex}.su2/" $caseNameO2
+sed -i "s/RESTART_SOL= NO/RESTART_SOL= YES/" $caseNameO2
+sed -i "s%SOLUTION_FILENAME= restart_solution.dat%SOLUTION_FILENAME= ../../O1/cfdG${meshIndex}/restart_flow_G${meshIndex}.dat%" $caseNameO2
+sed -i "s/MUSCL_FLOW= NO/MUSCL_FLOW= YES/" $caseNameO2
+sed -i "s/VENKAT_LIMITER_COEFF= 0.01/VENKAT_LIMITER_COEFF= ${secondOrderTVD}/" $caseNameO2
+
+# Running SU2
+echo Starting O2 simulation... 
+echo Running with $optimalCoreNumber cores
+mpirun -n $optimalCoreNumber SU2_CFD $caseNameO2 #>"logG${meshIndex}.log"
+	
+# Back to master folder
+cd ..
+cd ..
--- Codes/bashCodes/polarComputation.sh
+++ Codes/bashCodes/polarComputation.sh
@@ -1,18 +1,29 @@
-# The following script produces the polar of the RAE2822 airfoil.
-# Place the script in the directory in which you want to generate
-# the files of the simulations. In the same folder there should be:
-# - the config file named "simulationCase.cfg". The configuration
-#   file shall have all standard entries for the output filenames.
-#   Moreover, the parameters which are going to be changed during
-#   the iterations shall be named according to the searchStrings
-#   of the sed command (this version changes the AoA only).
-# - a "mesh" folder containing a "mesh.su2" file.
+echo "Polar computation initialized initialized"
+
+read -p "Input the mesh numbering index: " meshIndex
+read -p "Input the maximum number of cores allowed: " maxCoreNumber
+read -p "Input tha name of the template configuration file: " templateName
+read -p "Select either first or second order: " simulationOrder
+
+extFolderName="O1"
+
+if [ $simulationOrder -eq 2 ] ; then
+    echo "Second order analysis selected!"
+	read -p "Input the TVD coefficient for meshG${meshIndex}: " TVD
+	extFolderName="O2"
+fi
 
 # Definition of the angles of attack of interest
-aoaArray=(0 2 4 6)
+aoaArray=(m4 m2 0 2 4 6 8 10 12 13 14 145 15)
+vXArray=(9.49681 9.5142 9.52 9.5142 9.49681 9.46785 9.42735 9.37537 9.31197 9.2760 9.23722 9.2168 9.1956)
+vYArray=(-0.33224 -0.33224 0.0 0.33224 0.66408 0.99511 1.32493 1.65313 1.97932 2.1415 2.30310 2.3836 2.4640)
 
-# Definition of the maximum allowed cores
-maxCoreNumber=6
+# Creating folder for the selected order, copy .cfg inside and moving inside
+mkdir $extFolderName
+cd $extFolderName
+
+# Copy mesh folder
+cp -r "../../../../MeshFiles/meshG${meshIndex}" "meshG${meshIndex}"
 
 # Computing iterations number
 nPoints=${#aoaArray[@]}
@@ -21,38 +32,47 @@ iter=$(echo "scale=0;$nPoints-1" | bc)
 # Iterative setting of the test case
 for jj in $(seq 0 1 $iter) ; do 
 
+	echo "Simulation for AOA ${aoaArray[$jj]}..."
+
 	# Extracting current angle of attack
 	currentAoa=${aoaArray[$jj]}
 
 	# Generating the test case folder
-	caseFolderName="caseAoa${currentAoa}"
+	caseFolderName="case_A${currentAoa}"
 	mkdir $caseFolderName  
 	
 	# Generating simulation recup file
-	cp simulationCase.cfg $caseFolderName
+	cp "../${templateName}" $caseFolderName/
 	cd $caseFolderName
-	caseName="simulationCase_aoa${currentAoa}.cfg"
-	mv simulationCase.cfg $caseName
 	
 	# Updating the angle of attack in the test case file
-	sed -i "s/AOA= 0.0974/AOA= ${currentAoa}/" $caseName
+	sed -i "s/INC_VELOCITY_INIT= ( 0.0, 0.0, 0.0 )/INC_VELOCITY_INIT= ( ${vXArray[$jj]}, ${vYArray[$jj]}, 0.0)/" $templateName
 	
 	# Updating output files name
-	sed -i "s/CONV_FILENAME= history/CONV_FILENAME= history_aoa${currentAoa}/" $caseName
-	sed -i "s/RESTART_FILENAME= restart_flow.dat/RESTART_FILENAME= restart_flow_aoa${currentAoa}.dat/" $caseName
-	sed -i "s/VOLUME_FILENAME= flow/VOLUME_FILENAME= flow_aoa${currentAoa}/" $caseName
-	sed -i "s/SURFACE_FILENAME= surface_flow/SURFACE_FILENAME= surface_flow_aoa${currentAoa}/" $caseName
-	sed -i "s/MESH_OUT_FILENAME= mesh_out.su2/MESH_OUT_FILENAME= mesh_out_aoa${currentAoa}.su2/" $caseName
+	sed -i "s/CONV_FILENAME= history/CONV_FILENAME= history_aoa${currentAoa}/" $templateName
+	sed -i "s/RESTART_FILENAME= restart_flow.dat/RESTART_FILENAME= restart_flow_aoa${currentAoa}.dat/" $templateName
+	sed -i "s/VOLUME_FILENAME= flow/VOLUME_FILENAME= flow_aoa${currentAoa}/" $templateName
+	sed -i "s/SURFACE_FILENAME= surface_flow/SURFACE_FILENAME= surface_flow_aoa${currentAoa}/" $templateName
+	sed -i "s/MESH_OUT_FILENAME= mesh_out.su2/MESH_OUT_FILENAME= mesh_out_aoa${currentAoa}.su2/" $templateName
+	sed -i "s%MESH_FILENAME= ../mesh/mesh.su2%MESH_FILENAME= ../meshG${meshIndex}/meshG${meshIndex}.su2%" $templateName
 	
+	if [ $simulationOrder -eq 2 ] ; then
+		sed -i "s/RESTART_SOL= NO/RESTART_SOL= YES/" $templateName
+		sed -i "s/MUSCL_FLOW= NO/MUSCL_FLOW= YES/" $templateName
+    	sed -i "s/VENKAT_LIMITER_COEFF= 0.01/VENKAT_LIMITER_COEFF= ${TVD}/" $templateName
+		sed -i "s%SOLUTION_FILENAME= restart_solution.dat%SOLUTION_FILENAME= ../../O1/case_A${currentAoa}/restart_flow_aoa${currentAoa}.dat%" $templateName
+	fi
+
 	# Computing optimal core number
-	elemNumber=$(sed -n 's/^NELEM= \(.*\)/\1/p' < ../mesh/mesh.su2)
+	elemNumber=$(sed -n 's/^NELEM= \(.*\)/\1/p' < ../meshG${meshIndex}/meshG${meshIndex}.su2)
 	optimalCoreNumber=$(echo "scale=0;($elemNumber+10000)/20000" | bc)
 	if [ $optimalCoreNumber -gt $maxCoreNumber ] ; then
 		optimalCoreNumber=$maxCoreNumber
 	fi
 	
 	# Running SU2
-	mpirun -n $coreNumber SU2_CFD $caseName # >"logAoa${currentAoa}.log"
+	echo "Number of core employed: ${optimalCoreNumber}"
+	mpirun -n $optimalCoreNumber SU2_CFD $templateName #>"logAoa${currentAoa}.log"
 	
 	# Back to master folder
 	cd ..
